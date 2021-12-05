5e3382fc99bd8765c829434d67fa7f97d727c49f
gugohh <oohhgug@gmail.com>
2021-12-05 22:22:22 +0100

    initial state and  adding our product images

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
+  state = {
+    productos : [
+      {name:'Tomate' ,price:170 ,img:'/productos/tomate.jpeg',},
+      {name:'Arbejas' ,price:240 ,img:'/productos/arbejas.jpeg',},
+      {name:'Lechuga' ,price: 199,img:'/lechuga/lechuga.jpeg',},
+    ]
+  }
+
   render() {
 
     return (
